9bee9727f0330e90e4e71150f99005bec1ac92ba
Heegun <khg878@gmail.com>
2021-03-23 13:08:43 +0900

    백트래킹

---
+++ boj/bp/boj_G5_감소하는수_Heegun_re_백트래킹.py
@@ -0,0 +1,44 @@
+"""https://www.acmicpc.net/problem/1038"""
+
+
+"""감소하는 수"""
+
+
+n = int(input())
+
+def solution(n):
+    cnt = 0
+    num = 1
+
+    while True:
+        str_num = str(num)
+        flag = True
+
+        if len(str_num) == 1:
+            pass
+        else:
+            for i in range(1,len(str_num)):
+                if int(str_num[i]) < int(str_num[i-1]):
+                    continue
+                else:
+                    start = str_num[:i-1]
+                    mid = str(int(str_num[i-1]) + 1)
+                    end = '0' + str_num[i+1:]
+                    num = int(start + mid + end)
+                    flag = False
+                    break
+        
+        if flag:
+            cnt += 1
+            if cnt == n:
+                return num
+            num += 1
+
+
+
+if n >= 1023:
+    print(-1)
+elif n == 0:
+    print(0)
+else:
+    print(solution(n))
